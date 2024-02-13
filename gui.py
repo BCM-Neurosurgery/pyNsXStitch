ffd9275eef9ac81c097f24b73720ffb3465c5c04
TomekFraczek <tfraczek@uw.edu>
2024-02-13 15:23:37 -0800

    Overhaul time table, color working

--- gui.py
+++ gui.py
@@ -21,15 +21,21 @@ class StitcherGUI(object):
         self.file_name = tk.StringVar(self.root, value='stitched')
         self.start_time = tk.DoubleVar(master=self.root, value=None)
         self.end_time = tk.DoubleVar(master=self.root, value=None)
+        self.start_timestamp = tk.IntVar(master=self.root, value=None)
+        self.end_timestamp = tk.IntVar(master=self.root, value=None)
 
         # Additional important values to keep track of
         self.full_comment_df = self.init_comments()
         self.comment_df = self.init_comments()
         self.streamed_files = None
+        self.ts_freq = None
+        self.nev_start = None
 
         # Additional GUI elements
         self.time_frame = None
         self.comment_frame = None
+        self.table_elements = []
+        self.table_area = None
 
         self.build_gui()
         self.root.mainloop()
@@ -91,10 +97,10 @@ class StitcherGUI(object):
         browse.pack(side=tk.LEFT)
 
     def build_mid_row(self):
-        middle_row = tk.Frame(master=self.root)
+        middle_row = tk.Frame(master=self.root, pady=20)
 
         time_select = self.build_time_select(middle_row)
-        time_select.pack(side=tk.LEFT, anchor=tk.N)
+        time_select.pack(side=tk.LEFT, anchor=tk.N,)
         # info_column = self.build_info_column(middle_row)
         # info_column.pack(side=tk.LEFT, anchor=tk.N)
 
@@ -116,7 +122,7 @@ class StitcherGUI(object):
         tk.Label(master=inputs, text="Start Time").pack(side=tk.TOP, anchor=tk.W, pady=(20, 0))
         tk.Entry(master=inputs, textvariable=self.start_time).pack(side=tk.TOP)
         tk.Label(master=inputs, text="End Time").pack(side=tk.TOP, anchor=tk.W)
-        tk.Entry(master=inputs, textvariable=self.start_time).pack(side=tk.TOP)
+        tk.Entry(master=inputs, textvariable=self.end_time).pack(side=tk.TOP)
         buttons = tk.Frame(master=inputs)
         tk.Button(master=buttons, text='Update', command=self.update_tlims).pack(side=tk.RIGHT, anchor=tk.N)
         tk.Button(master=buttons, text='Reset', command=self.reset_tlims).pack(side=tk.RIGHT, anchor=tk.N)
@@ -131,37 +137,118 @@ class StitcherGUI(object):
 
         return self.time_frame
 
+    def get_row_color(self, row_idx, row_ts):
+        row_color = ['gray85', 'gray90'][row_idx % 2]
+        if self.start_timestamp.get() == row_ts:
+            row_color = 'palegreen'
+        elif self.end_timestamp.get() == row_ts:
+            row_color = 'lightcoral'
+        elif self.start_timestamp.get() < row_ts < self.end_timestamp.get():
+            row_color = ['SkyBlue1', 'SteelBlue1'][row_idx % 2]
+        return row_color
+
     def build_comment_table(self):
-        table_frame = tk.Frame(master=self.comment_frame)
-        col_widths = [15, 15, 30]
+
+        col_widths = [100, 100, 250, 30, 30]
+        col_starts = [10, 110, 210, 460, 490]
+        col_height = 25
+        y_offset = 10
+        bttn_offset = 7
+        canvas_width = sum(col_widths)
+        canvas_height = col_height * (1+len(self.comment_df))
+
+        self.table_area = tk.Canvas(
+            self.comment_frame,
+            scrollregion=(0, 0, canvas_width, canvas_height),
+            width=canvas_width, height=500,
+            yscrollcommand=self.scroll.set
+        )
+
         # Build the headers:
-        header = tk.Frame(master=table_frame)
         for i, col in enumerate(self.comment_df.columns):
-            tk.Label(header, text=col, width=col_widths[i]).pack(side=tk.LEFT)
-        header.pack(side=tk.TOP, anchor=tk.W)
+            self.table_area.create_text(col_starts[i], y_offset,  text=col, width=col_widths[i], anchor=tk.NW)
+        self.table_area.create_text(
+            col_starts[3]-15, y_offset,
+            text=r'Start\Stop',
+            width=sum(col_widths[3:]),
+            anchor=tk.NW)
 
         # For each entry in the comments table, add an entry, and build the appropriate selectors
         for i, row_data in enumerate(self.comment_df.iterrows()):
-            row_frame = tk.Frame(master=table_frame)
+            row_items = []
+            row_ts = row_data[1]['Timestamp']
+            row_color = self.get_row_color(i, row_ts)
+            row_yloc = y_offset + col_height * (1+i)
+
+            bg_rectange = self.table_area.create_rectangle(
+                0, row_yloc-5, canvas_width, row_yloc+col_height-5, fill=row_color, outline=''
+            )
+            row_items.append(bg_rectange)
+
             for j, value in enumerate(row_data[1]):
                 text = self.comment_df.iloc[i, j]
-                tk.Label(master=row_frame, text=text, width=col_widths[j]).pack(side=tk.LEFT, anchor=tk.W)
-            row_frame.pack(side=tk.TOP, anchor=tk.W)
-        table_frame.pack(side=tk.LEFT)
+                self.table_area.create_text(col_starts[j], row_yloc,  text=text, width=col_widths[j], anchor=tk.NW)
+
+            buttn_y = row_yloc + bttn_offset
+            start = tk.Radiobutton(
+                self.table_area,
+                variable=self.start_timestamp,
+                value=row_ts,
+                command=self.update_ts_lims,
+                background=row_color
+            )
+            self.table_area.create_window(col_starts[3], buttn_y, window=start)
+            stop = tk.Radiobutton(
+                self.table_area,
+                variable=self.end_timestamp,
+                value=row_ts,
+                command=self.update_ts_lims,
+                background=row_color
+            )
+            self.table_area.create_window(col_starts[4], buttn_y, window=stop)
+            row_items.append(start)
+            row_items.append(stop)
+            self.table_elements.append(row_items)
+        self.table_area.pack(side=tk.LEFT)
+        self.scroll.config(command=self.table_area.yview)
+
+    def recolor_table(self):
+        for rox_idx, row_elements in enumerate(self.table_elements):
+            row_ts = self.comment_df.iloc[rox_idx, 0]
+            row_color = self.get_row_color(rox_idx, row_ts)
+            self.table_area.itemconfigure(row_elements[0], fill=row_color)
+            for button in row_elements[1:]:
+                button.configure(background=row_color)
+
+    def update_ts_lims(self):
+        self.recolor_table()
 
     def update_tlims(self):
-        pass
+        """Find the narrowest pair of timestamps that contain the time limits in seconds"""
+        time_lims = np.array([self.start_time.get(), self.end_time.get()])
+        approx_ts_lims = (time_lims * self.ts_freq) + self.nev_start
+        self.start_timestamp.set(np.floor(approx_ts_lims[0]))
+        self.end_timestamp.set(np.ceil(approx_ts_lims[1]))
+
+        self.recolor_table()
 
     def reset_tlims(self):
         pass
 
     def search_comments(self):
-        # TODO: implement the search over self.full_comment_df and put result into self.comment_df
-        self.build_comment_table()
+        search_text = self.search_text.get()
+        have_match = np.array([
+            search_text in comment
+            for comment in self.full_comment_df['Comment']
+        ])
+        self.comment_df = self.full_comment_df.iloc[have_match, :]
+        self.rebuild_comments()
 
     def reset_comments(self):
         self.comment_df = self.full_comment_df
+        self.rebuild_comments()
 
+    def rebuild_comments(self):
         if self.comment_frame:
             self.comment_frame.destroy()
 
@@ -177,11 +264,11 @@ class StitcherGUI(object):
         self.streamed_files = get_all_streamed_files(self.source_dir.get(), full_paths=True)
         raw_df = get_all_nev_comments(self.streamed_files['NeV'])
         first_nev = NevFile(self.streamed_files['NeV'][0])
-        nev_start = get_nev_rec_start(first_nev)
-        ts_freq = first_nev.basic_header['TimeStampResolution']
+        self.nev_start = get_nev_rec_start(first_nev)
+        self.ts_freq = first_nev.basic_header['TimeStampResolution']
         cleaned_df = pd.DataFrame.from_dict({
             'Timestamp': raw_df['TimeStamps'],
-            'Time Elapsed': np.round((raw_df['TimeStamps'] - nev_start) / ts_freq, 2),
+            'Time Elapsed': np.round((raw_df['TimeStamps'] - self.nev_start) / self.ts_freq, 2),
             'Comment': raw_df['Data']
         })
         self.full_comment_df = cleaned_df
