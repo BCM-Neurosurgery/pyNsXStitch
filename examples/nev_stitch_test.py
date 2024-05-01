90b674dea9f89676b322b3d3176b332ac3b391bb
redffish <he279@wisc.edu>
2024-05-01 05:05:35 -0500

    Update test directory path

--- examples/nev_stitch_test.py
+++ examples/nev_stitch_test.py
@@ -1,18 +1,24 @@
+import sys
 import os
 import numpy as np
 import pandas as pd
-from pyNsXStitch.stiching import StitchedNeVFile, NevFile
+
+# Get the absolute path of `pyNsXStitch` module
+abs_module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(abs_module_path)
+
+from pyNsXStitch.stitchers import StitchedNeVFile, NevFile
 from pyNsXStitch.helpers import get_nev_rec_start
 
-source_dir = r'D:\Work\DataNet\TestData\sources\blackrock\20240125-144340'
-out_path = r'D:\Work\DataNet\TestData\sources\blackrock\test.nev'
+source_dir = r'../../nev-only-dual-nsp/'
+out_path = r'../../test/test.nev'
 
 files = os.listdir(source_dir)
 nev_filenames = [os.path.join(source_dir, f) for f in files if '.nev' in f]
 
 stitch = StitchedNeVFile(nev_filenames)
 with open(out_path, 'wb') as out_file:
-    stitch.write_data(out_file)
+    stitch.write(out_file)
 
 test_out = NevFile(out_path)
 rec_start = get_nev_rec_start(NevFile(nev_filenames[0]))
