86250a8c1361e5128a382365c78e7dc5aaff847f
TomekFraczek <tfraczek@uw.edu>
2024-08-14 16:21:53 +0200

    Remove Jiaqui's debug source dir

--- gui.py
+++ gui.py
@@ -192,7 +192,7 @@ class StitcherGUI(object):
         self.root = self.init_window()
 
         # Variables used for tracking the GUI state
-        self.source_dir = tk.StringVar(self.root, value="/Users/_astoria/bcm/TestData-Jiaqi")
+        self.source_dir = tk.StringVar(self.root, value=None)
         self.output_dir = tk.StringVar(self.root, value=None)
         self.search_text = tk.StringVar(self.root, value='')
         self.file_name = tk.StringVar(self.root, value='stitched')
