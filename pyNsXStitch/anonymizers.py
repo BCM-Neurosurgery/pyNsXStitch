43e2c8c3cdb8986e325affb0c464c64b4c5d0ea9
TomekFraczek <tfraczek@uw.edu>
2026-08-19 11:51:55 -0500

    Significantly expanded anonymizer functions to add stripping of dates

--- pyNsXStitch/anonymizers.py
+++ pyNsXStitch/anonymizers.py
@@ -1,13 +1,31 @@
+import numpy as np
+import pandas as pd
 from brpylib import NsxFile, NevFile
+from helpers import write_one_nev_file, write_one_nsx_file
 
 AUDIO_CHAN_NAMES = [
     'mic',
     'audio'
 ]
 
+def random_date_offset():
+    """Create a random datetime offset +- 31 years"""
+    return pd.Timedelta(np.random.uniform(-10**9, 10**9), unit='s')
+
+def scramble_date(date, offset=None):
+    """
+    Offset the date by a random amount of time.
+
+    Using a consistent random offset for a whole visit would preserver order of files, but scramble the PHI
+    """
+    if offset is None:
+        offset = random_date_offset()
+    new_date = pd.Timestamp(date) + offset
+    return new_date.to_pydatetime()
+
 
 def remove_audio_nsx(nsx_file: NsxFile, output_filename: str):
-
+    """Room audio can contain snippets of conversation that make it PHI"""
     non_audio_channels = []
     for i, metadata in enumerate(nsx_file.extended_headers):
         for forbidden in AUDIO_CHAN_NAMES:
@@ -18,3 +36,43 @@ def remove_audio_nsx(nsx_file: NsxFile, output_filename: str):
             non_audio_channels.append(i)
 
     return nsx_file.savesubsetnsx(non_audio_channels, out_file=output_filename)
+
+
+def remove_dates_nev(nev_file: NevFile, output_filename: str|None = None, date_offset: pd.Timedelta|None =None):
+    """UTC dates in the NeV file are PHI"""
+    date_offset = random_date_offset() if date_offset is None else date_offset
+    nev_file.basic_header['TimeOrigin'] = scramble_date(nev_file.basic_header['TimeOrigin'], date_offset)
+    if output_filename:
+        write_one_nev_file(nev_file, output_filename)
+    return nev_file
+
+
+def remove_dates_nsx(nsx_file: NsxFile, output_filename: str|None = None, date_offset: pd.Timedelta|None =None):
+    """UTC dates in the NsX files are PHI since they imply admission/clinical visit dates"""
+
+    date_offset = random_date_offset() if date_offset is None else date_offset
+    nsx_file.basic_header['TimeOrigin'] = scramble_date(nsx_file.basic_header['TimeOrigin'], date_offset)
+
+    if output_filename:
+        write_one_nsx_file(nsx_file, output_filename)
+    return nsx_file
+
+
+def nev_anonymize(nev_file: NevFile, output_filename: str):
+    """
+    Wrapper to apply several of the commonly used anonymization functions to an NeV file
+
+    This is not a guarantee that all PHI is removed, and you should review the data yourself
+    """
+
+    no_dates_nev = remove_dates_nev(nev_file)
+    write_one_nev_file(no_dates_nev, output_filename)
+
+
+def nsx_anonymize(nsx_file: NsxFile, output_filename: str):
+
+    no_dates_nsx = remove_dates_nsx(nsx_file)
+    remove_audio_nsx(no_dates_nsx, output_filename) # This also writes the file
+
+
+
